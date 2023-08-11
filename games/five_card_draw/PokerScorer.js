c1335df2c67c793620ac558d9595a022a15f6d20
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-08-11 03:59:00 +0100

    Fix poker straight check
    Refactored poker scoring checks for readability/reduce repetition.
    Added two simple misc. slash commands.

--- games/five_card_draw/PokerScorer.js
+++ games/five_card_draw/PokerScorer.js
@@ -12,29 +12,50 @@ class PokerScorer {
         highCard: 0,
     };
 
+    /*
+        Unsure if better way to write this more readably?
+        Issue is each check is a unique type of check with a specific name.
+        Could possibly combine some of the repetitive functions into one where
+        the numbers for lengths are passed in as args but then comments would be
+        required for readability/naming - better to just keep this way?
+    */
     static scoreHand(hand) {
         const values = hand.map((card) => card.value);
         const suits = hand.map((card) => card.suit);
 
+        const duplicateNumbers = values.filter(
+            (value) => values.indexOf(value) !== values.lastIndexOf(value)
+        );
+
+        // prettier-ignore
         if (PokerScorer.isStraightFlush(values, suits) && values.every((value) => value > 9)) {
             return ['ROYAL FLUSH!!!', PokerScorer.multipliers.royalFlush];
-        } else if (PokerScorer.isStraightFlush(values, suits)) {
+        }
+        else if (PokerScorer.isStraightFlush(values, suits)) {
             return ['Straight Flush!', PokerScorer.multipliers.straightFlush];
-        } else if (PokerScorer.isFourOfAKind(values)) {
+        }
+        else if (PokerScorer.isFourOfAKind(values, duplicateNumbers)) {
             return ['Four of a Kind!', PokerScorer.multipliers.fourOfAKind];
-        } else if (PokerScorer.isFullHouse(values)) {
+        }
+        else if (PokerScorer.isFullHouse(values, duplicateNumbers)) {
             return ['Full House!', PokerScorer.multipliers.fullHouse];
-        } else if (PokerScorer.isFlush(suits)) {
+        }
+        else if (PokerScorer.isFlush(suits)) {
             return ['Flush!', PokerScorer.multipliers.flush];
-        } else if (PokerScorer.isStraight(values)) {
+        }
+        else if (PokerScorer.isStraight(values)) {
             return ['Straight!', PokerScorer.multipliers.straight];
-        } else if (PokerScorer.isThreeOfAKind(values)) {
+        }
+        else if (PokerScorer.isThreeOfAKind(values, duplicateNumbers)) {
             return ['Three of a Kind!', PokerScorer.multipliers.threeOfAKind];
-        } else if (PokerScorer.isTwoPair(values)) {
+        }
+        else if (PokerScorer.isTwoPair(values, duplicateNumbers)) {
             return ['Two Pair', PokerScorer.multipliers.twoPair];
-        } else if (PokerScorer.isOnePair(values)) {
+        }
+        else if (PokerScorer.isOnePair(values)) {
             return ['One Pair', PokerScorer.multipliers.onePair];
-        } else {
+        }
+        else {
             return ['Bad luck this time...', PokerScorer.multipliers.highCard];
         }
     }
@@ -43,45 +64,33 @@ class PokerScorer {
         return [...new Set(values)].length === 4;
     }
 
-    static isTwoPair(values) {
-        const duplicateNumbers = values.filter(
-            (value) => values.indexOf(value) !== values.lastIndexOf(value)
-        );
+    static isTwoPair(values, duplicateNumbers) {
         return [...new Set(values)].length === 3 && [...new Set(duplicateNumbers)].length === 2;
     }
 
-    static isThreeOfAKind(values) {
-        const duplicateNumbers = values.filter(
-            (value) => values.indexOf(value) !== values.lastIndexOf(value)
-        );
+    static isThreeOfAKind(values, duplicateNumbers) {
         return [...new Set(values)].length === 3 && [...new Set(duplicateNumbers)].length === 1;
     }
 
     static isStraight(values) {
         const sortedNumbers = values.toSorted((a, b) => a - b);
-        for (let i = 0; i < values.length - 1; i++) {
-            if (sortedNumbers[i + 1] !== sortedNumbers[i] + 1) {
-                return false;
-            }
-        }
-        return true;
+
+        // covers all valid straight combinations - A-5 separate as A has a value of 14 (ace-high)
+        const isFiveConsecutiveNumbers = sortedNumbers[4] - sortedNumbers[0] === 4;
+        const isAceThroughFive = [2, 3, 4, 5, 14].every((number, i) => number === sortedNumbers[i]);
+
+        return isFiveConsecutiveNumbers || isAceThroughFive;
     }
 
     static isFlush(suits) {
         return suits.every((suit) => suit === suits[0]);
     }
 
-    static isFullHouse(values) {
-        const duplicateNumbers = values.filter(
-            (value) => values.indexOf(value) !== values.lastIndexOf(value)
-        );
+    static isFullHouse(values, duplicateNumbers) {
         return [...new Set(values)].length === 2 && [...new Set(duplicateNumbers)].length === 2;
     }
 
-    static isFourOfAKind(values) {
-        const duplicateNumbers = values.filter(
-            (value) => values.indexOf(value) !== values.lastIndexOf(value)
-        );
+    static isFourOfAKind(values, duplicateNumbers) {
         return [...new Set(values)].length === 2 && [...new Set(duplicateNumbers)].length === 1;
     }
 
