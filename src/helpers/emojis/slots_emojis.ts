49daa728dfe93af16362b7762e70eb3abd584e64
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-17 02:47:25 +0000

    Migrate emojis files

---
+++ src/helpers/emojis/slots_emojis.ts
@@ -0,0 +1,11 @@
+export const dougEmoji = '1105890013297791036';
+
+export const barFalse = '<:BarFalse:1097894553740116109>';
+export const rip = '<:RIP:1091768169166819498>';
+export const scratch = '<:Scratch:1091766171608555622>';
+export const rickroll = '<:rickroll_static:1092852857562218638>';
+export const arthur = '<:arthur:1109122456884613130>';
+export const python = '<:python:1091395113843957760>';
+export const barTrue = '<:BarTrue:1097894803720654979>';
+export const liquidDrinkers = '<:liquiddrinkers:1097901807545626715>';
+export const doug = `<:douglloyd:${dougEmoji}>`;
