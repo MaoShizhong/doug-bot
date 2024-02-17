44c929fcfabd0a13f50eae4a0e4aa0209f26239d
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-17 01:42:18 +0000

    Rewrite slash command deploy file with typescript
    Defined SlashCommand type

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,43 +1,48 @@
-const { REST, Routes } = require('discord.js');
-const { clientID, token } = require('../config.json');
-const fs = require('node:fs');
-const path = require('node:path');
+import { REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
+import { readdirSync } from 'fs';
+import { join as pathJoin } from 'path';
+import { SlashCommand } from './types';
 
-const commands = [];
+const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
+
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const foldersPath = pathJoin(__dirname, 'commands');
+const commandFolders = readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
     // Grab all the command files from the commands directory you created earlier
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+    const commandsPath = pathJoin(foldersPath, folder);
+    const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
 
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+        const filePath = pathJoin(commandsPath, file);
+        const command: SlashCommand = await import(filePath);
+
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
         } else {
-            console.log(
+            console.error(
                 `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
             );
         }
     }
 }
-x;
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const { CLIENT_ID, BOT_TOKEN } = process.env;
+
+if (!CLIENT_ID || !BOT_TOKEN) throw new Error('No client ID or bot token!');
+
+const rest = new REST().setToken(BOT_TOKEN);
 
 // and deploy your commands!
-(async () => {
+(async (): Promise<void> => {
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         // The put method is used to fully refresh all commands globally
-        const data = await rest.put(Routes.applicationCommands(clientID), { body: commands });
+        const data = await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands }) as unknown[];
 
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
