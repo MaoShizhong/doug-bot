fc01d7d4e128b356e141798004adc3c5d17affdb
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-18 07:32:17 +0000

    Delete old reference files

--- old/doug/dick_butt.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { dickbutt } = require('../../images/dickbutt.js');
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('nsfw').setDescription(':)'),
-    async execute(interaction) {
-        await interaction.reply(dickbutt);
-    },
-};
