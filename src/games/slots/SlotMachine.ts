12b8053d7ec810bc877e9d12e72bd3d0b5c91f5c
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-18 04:52:15 +0000

    Refactor slots files with TypeScript

---
+++ src/games/slots/SlotMachine.ts
@@ -0,0 +1,94 @@
+import { EmbedBuilder } from 'discord.js';
+import { LUCKY_STAR_CHANCE } from '../../constants/constants';
+import { UserModel } from '../../db/models/User';
+import { SlotEmoji } from '../../types';
+import { Reel } from './Reel';
+import { Scorer } from './Scorer';
+import { addBetResultsToEmbed, getInitialSlotsEmbed } from './slots_embed';
+
+export class SlotMachine {
+    NUMBER_OF_REELS: number;
+    EMOJIS_PER_REEL: number;
+    score: number;
+    reels: SlotEmoji[][];
+    reelOptions: SlotEmoji[];
+    player: UserModel;
+    bet: number | null;
+    isLuckyStar: boolean;
+    scorer: Scorer;
+
+    constructor(player: UserModel, bet: number | null) {
+        this.NUMBER_OF_REELS = 5;
+        this.EMOJIS_PER_REEL = 3;
+        this.score = 0;
+        this.reels = Array(this.NUMBER_OF_REELS)
+            .fill(null)
+            .map(() => Array(this.EMOJIS_PER_REEL));
+        this.reelOptions = Reel.options;
+        this.player = player;
+        this.bet = bet;
+        this.isLuckyStar = Math.random() < LUCKY_STAR_CHANCE;
+        this.scorer = new Scorer();
+    }
+
+    getSlotsResults(): EmbedBuilder {
+        this.playSlots();
+        if (this.score && this.isLuckyStar) this.score *= 2;
+
+        const resultsEmbed = getInitialSlotsEmbed(
+            this.player,
+            this.reels,
+            this.scorer.matches,
+            this.score,
+            this.isLuckyStar
+        );
+
+        if (this.bet) {
+            const earnings = this.bet * this.score;
+            addBetResultsToEmbed(resultsEmbed, this.bet, earnings, this.player.goldString);
+
+            (this.player.gold as number) += earnings;
+        }
+
+        return resultsEmbed;
+    }
+
+    /**
+     * Spin and score - provide increased rates for scoring if low total gold
+     * Increasing any of the following 3 parameters to increase rates
+     */
+    playSlots(): void {
+        const maxPlays = 2;
+        const chanceOfRespinInPercent = 80;
+
+        const increasedRates =
+            this.player.hasLowGold && Math.random() < chanceOfRespinInPercent / 100;
+
+        let plays = 0;
+
+        do {
+            this.spinReels();
+            this.score = this.scorer.scoreWinningLines(this.reels);
+            plays++;
+        } while (!this.score && increasedRates && plays < maxPlays);
+    }
+
+    spinReels(): void {
+        for (let i = 0; i < this.NUMBER_OF_REELS; i++) {
+            this.shuffleReels(this.reelOptions);
+
+            for (let j = 0; j < this.EMOJIS_PER_REEL; j++) {
+                this.reels[i][j] = this.reelOptions[j];
+            }
+        }
+    }
+    /**
+     * Fisher-Yates shuffle, less permutation-bias than other shuffle algos
+     */
+    shuffleReels(arr: SlotEmoji[]): void {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+}
---
+++ src/games/slots/slots_embed.ts
@@ -0,0 +1,136 @@
+import { EmbedBuilder } from 'discord.js';
+import { gold } from '../../constants/emojis/general_emojis';
+import {
+    arthur,
+    barFalse,
+    barTrue,
+    doug,
+    liquidDrinkers,
+    python,
+    rickroll,
+    rip,
+    scratch,
+} from '../../constants/emojis/slots_emojis';
+import { UserModel } from '../../db/models/User';
+import { SlotEmoji } from '../../types';
+import { Match, MatchInfo } from './Scorer';
+
+function renderReels(reels: SlotEmoji[][]): string {
+    const space = '\u2004\u2004';
+
+    const leftReelEdge = `|${space}`;
+    const midReelEdges = `${space}|${space}`;
+    const rightReelEdge = `${space}|`;
+
+    let results = '';
+
+    for (let i = 0; i < reels[0].length; i++) {
+        results += `${leftReelEdge}${reels[0][i].emoji}${midReelEdges}${reels[1][i].emoji}${midReelEdges}${reels[2][i].emoji}${midReelEdges}${reels[3][i].emoji}${midReelEdges}${reels[4][i].emoji}${rightReelEdge}\n`;
+    }
+
+    return results;
+}
+
+/*
+    For showing individual matches (line #, emojis, value) in the results embed
+*/
+function renderMatchLine(matchLine: string, emojis: SlotEmoji[]): string {
+    let str = `Line ${matchLine} `;
+
+    emojis.forEach((matchedEmoji): void => {
+        str += matchedEmoji.emoji;
+    });
+
+    const matchQuantity = `match${emojis.length}` as Match;
+    str += ` ${emojis[0][matchQuantity]} points`;
+
+    return str;
+}
+
+export function getInitialSlotsEmbed(
+    user: UserModel,
+    reels: SlotEmoji[][],
+    matches: MatchInfo[],
+    score: number,
+    isLuckyStar: boolean
+): EmbedBuilder {
+    const renderedReels = renderReels(reels);
+
+    const resultsEmbed = new EmbedBuilder()
+        .setColor(user.profileColor)
+        .setTitle('Slots!')
+        .addFields(
+            {
+                name: 'Rolling slots!',
+                value: 'Match 3, 4 or 5 (starting from the first reel\nor ending with the last reel) to score points!\nUse `/slotpatterns` to see the 11 match lines.',
+            },
+            {
+                name: 'Symbols:',
+                value: `${barFalse} ${rip} ${scratch} **Match 3/4/5**: 1 / 5 / 15\n${rickroll} ${arthur} **Match 3/4/5:** 4 / 15 / 45\n${python} ${barTrue} **Match 3/4/5:** 12 / 50 / 180\n${liquidDrinkers} **Match 3/4/5:** 25 / 125 / 500\n${doug} **Match 3/4/5:** 50 / 350 / 1200`,
+            },
+            {
+                name: '\u200b',
+                value: renderedReels,
+            }
+        );
+
+    matches.forEach(([emojis, patternNumber]): void => {
+        resultsEmbed.addFields({
+            name: '\u200b',
+            value: renderMatchLine(patternNumber, emojis),
+            inline: true,
+        });
+    });
+
+    addScoreFieldsToEmbed(resultsEmbed, score, isLuckyStar);
+    return resultsEmbed;
+}
+
+// For score announcement in results embed
+function addScoreFieldsToEmbed(embed: EmbedBuilder, score: number, isLuckyStar: boolean): void {
+    // Changes score announcement tone depending on score
+    const extraMessages = {
+        '...what the...': 400,
+        '...holy hell!': 280,
+        '! Wow!': 125,
+        '! Really nice!': 45,
+        '! Nice!': 15,
+        '!': 1,
+    };
+
+    let extraMessage = '...';
+    for (const [message, threshold] of Object.entries(extraMessages)) {
+        if (score >= threshold) {
+            extraMessage = message;
+            break;
+        }
+    }
+
+    embed.addFields({
+        name: isLuckyStar ? ':star: :star: **Lucky stars!** :star: :star:' : 'Score:',
+        value: isLuckyStar
+            ? `Your score of ${
+                  score / 2
+              } points has been doubled to **${score}** points${extraMessage}`
+            : `You scored **${score}** points${extraMessage}`,
+    });
+}
+
+export function addBetResultsToEmbed(
+    embed: EmbedBuilder,
+    bet: number,
+    earnings: number,
+    userGoldString: string
+): void {
+    embed.addFields(
+        { name: 'Bet placed', value: `${bet.toLocaleString('en-US')}${gold}`, inline: true },
+        {
+            name: 'Winnings (bet * score)',
+            value: `${earnings.toLocaleString('en-US')}${gold}`,
+            inline: true,
+        },
+        { name: 'Current gold', value: `${userGoldString}${gold}` }
+    );
+
+    return;
+}
