b447b7afa993c38d03f884518547e572e8fd57dd
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-10-11 20:34:46 +0100

    Format AI.ts

--- src/config/AI.ts
+++ src/config/AI.ts
@@ -3,6 +3,6 @@ import { ChatOpenAI } from '@langchain/openai';
 export const GPT_CLIENT = new ChatOpenAI({
     apiKey: process.env.OPENAI_KEY,
     maxTokens: 1900,
-    model: "gpt-4o-mini",
+    model: 'gpt-4o-mini',
     temperature: 0.5,
 });
